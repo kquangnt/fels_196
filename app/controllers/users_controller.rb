d19fd59b8e0d462ac372c396b660172c6025365d
kqnt <nguyentkquang@gmail.com>
2016-11-02 18:58:11 +0700

    Task logi/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show]
+  before_action :load_user, only: :show
+
+  def index
+    @users = User.order_date_desc.paginate page: params[:page],
+      per_page: Settings.per_page.user
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -6,8 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t "loggin_success"
-      redirect_to root_url
+      log_in @user
+      flash[:success] = t "sign_up_success"
+      redirect_to @user
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/sign_up", to: "users#new"
   post "/sign_up", to: "users#create"
-  resources :users, only: [:new, :create]
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :users
 end
